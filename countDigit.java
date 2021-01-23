6da8285999214cc19c120cfc8bba70908d2fee6a
Gaurav <kumargaurav6299@gmail.com>
2021-01-23 21:26:03 +0530

    added a new file

---
+++ countDigit.java
@@ -0,0 +1,16 @@
+class countDigit
+{
+public static void main(String args[])
+{
+String s="gaurav23";
+int count=0;
+for(int i=0;i<s.length();i++)
+{
+if(s.charAt(i)>=48 && s.charAt(i)<=57)
+{
+count+=1;
+}
+}
+System.out.println(count);
+}
+}
